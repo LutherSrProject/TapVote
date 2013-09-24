58d8c3806a7d6c97c6a4ecbc190e8fdde6a50b90
etingel <etingel+git@gmail.com>
2013-09-23 21:24:45 -0500

    More!

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var http = require("http");
+var url = require("url");
 
-function start() {
+function start(route) {
   function onRequest(request, response) {
+	var pathname = url.parse(request.url).pathname;
+	console.log("Request for " + pathname + " received.");
     console.log("Request received.");
+    
+    route(pathname);
+    
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World");
     response.end();
