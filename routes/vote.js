5d7ab147e7d58e698607ae730525bdb92fe60d83
etingel <etingel+git@gmail.com>
2013-09-26 22:55:48 -0500

    Finishing touches...

--- routes/vote.js
+++ routes/vote.js
@@ -1,13 +1,12 @@
 var database = require("../modules/database");
 var responses = require("../modules/responses");
 
-function vote(response, postData) {
+function vote(req, response) {
     //Test this endpoint with curl -d '{"vote": "a"}' -H "Content-Type: application/json" http://localhost:8000/vote
     console.log("[INFO] Request handler 'vote' was called.");
-
-    try {
-        data = JSON.parse(postData);
-    } catch (err) {
+    data = req.body;
+    if (!data['vote']) { //this can happen if the content-type isn't set correctly when you send raw JSON
+        err = new Error()
         err["httpStatus"] = 400;
         err["httpResponse"] = "400 Bad Request";
         err["friendlyName"] = "JSON parse error";
