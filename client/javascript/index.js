bcfbb384956d6e2bc18957f79534bf3ed4e405c7
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2013-09-26 21:06:21 -0500

    log ajax errors to console

--- client/javascript/index.js
+++ client/javascript/index.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
-	$("#voter").submit(function(event){
-		$.ajax({type:"POST", 
-				url:"/vote", 
-				data:'{"vote": "a"}', 
-				success: function(data){console.log(data)}
-				});
-		event.preventDefault();
-		return false;
-	});
+    $("#voter").submit(function(event){
+        $.ajax({type:"POST", 
+            url:"/vote", 
+            data:'{"vote": "a"}', 
+            success: function(data) { console.log(data); },
+            error: function(data) { console.log(data); }
+		});
+        event.preventDefault();
+        return false; 
+    });
 });
 
