70fce3a56c3183ceb9a970e32b4b644475ca8a12
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2013-09-28 12:20:45 -0500

    listen on port 80 so that we don't conflict with tapvote

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var app = express();
 
 
 // all environments
-app.set('port', process.env.PORT || 8000);
+app.set('port', process.env.PORT || 8080);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.favicon());
