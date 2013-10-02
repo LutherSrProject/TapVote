1bc02cc50d2510a9bd2d3b34ec5b842ca078e7fa
etingel <etingel+git@gmail.com>
2013-10-01 21:31:34 -0500

    Finished adding createSurvey

--- routes/createSurvey.js
+++ routes/createSurvey.js
@@ -1,9 +1,9 @@
 var database = require("../modules/database");
 var httpresponses = require("../modules/httpresponses");
 
-function creatSurvey(req, response) {
-    //Test this endpoint with curl -d "{ 'title':'\"Because clickers are SO 1999.\"', 'questions': [{'question': 'Which is best?', 'answers': ['Puppies', 'Cheese', 'Joss Whedon', 'Naps']}],'password':'supersecretpassword' }" -H "Content-Type: application/json" http://localhost:8000/vote
-    console.log("[INFO] Request handler 'creatSurvey' was called.");
+function createSurvey(req, response) {
+    //Test this endpoint with curl -d '{ "title":"\"Because clickers are SO 1999.\"", "questions": [{"question": "Which is best?", "answers": ["Puppies", "Cheese", "Joss Whedon", "Naps"]}],"password":"supersecretpassword" }' -H "Content-Type: application/json" http://localhost:8000/createSurvey
+    console.log("[INFO] Request handler 'createSurvey' was called.");
     data = req.body;
     if (!data['title']) { //this can happen if the content-type isn't set correctly when you send raw JSON
         err = new Error()
@@ -34,5 +34,5 @@ function creatSurvey(req, response) {
     });
 }
 
-exports.creatSurvey = creatSurvey;
+exports.createSurvey = createSurvey;
 
