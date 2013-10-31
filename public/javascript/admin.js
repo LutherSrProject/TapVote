db493a6e3834709532e6877f73201c90bc994df1
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2013-10-31 14:43:51 -0500

    get and display survey info and results

--- public/javascript/admin.js
+++ public/javascript/admin.js
@@ -19,12 +19,82 @@
 })(jQuery);
 
 
-$(function getSurveyResults() {
+$(function getSurveyInfo() {
     var survey = $.QueryString['survey'];
-    $.get('/getSurveyResults', {surveyId: survey}, displaySurveyResults);
+    //$.get('/getSurveyInfo', {surveyId: survey}, displaySurveyInfo);
+    $.ajax({
+        type:'GET',
+        url: '/getSurveyInfo',
+        data: {surveyId: survey},
+        success: displaySurveyInfo,
+        error: displayAjaxError
+    });
 });
 
+function displayAjaxError(error) {
+    console.log(error.statusText);
+    console.log(error.responseText);
+}
+
+function getSurveyResults() {
+    var survey = $.QueryString['survey'];
+    $.get('/getSurveyResults', {surveyId: survey}, displaySurveyResults);
+}
+
 function displaySurveyResults(results) {
+    $.each(results, function (key, value) {
+        var elementId = '#answer-' + key;
+        var resultSpan = $("<span></span>");
+        resultSpan.text(value);
+
+        $(elementId).append(resultSpan);
+    });
     console.log(results);
+}
+
+function displaySurveyInfo(results) {
+    var titleDiv = $("#survey-title");
+
+    if (results.length = 0) {
+        titleDiv.text("Unknown survey Id");
+        return;
+    }
+
+
+    var el = $("<h2></h2>");
+    el.text(results['title']);
+    titleDiv.append(el);
+
+    var questionsDiv = $("#survey-questions");
+    var questions = results['questions'];
+    $.each(questions, function (index, question) {
+        // create a div for each question in questions
+        var questionDiv = $("<div></div>");
+        questionDiv.attr('id', 'question-'+question['id']);
+        questionDiv.attr('class', 'question');
+
+        var questionTitle = $("<div></div>");
+        questionTitle.text(question['value']);
+        questionDiv.append(questionTitle);
+
+        var answers = question['answers'];
+        $.each(answers, function (index, answer) {
+            var answerDiv = $("<div></div>");
+            answerDiv.attr('id', 'answer-'+answer['id']);
+            answerDiv.attr('class', 'answer');
+
+            var questionValue = $("<div></div>");
+            questionValue.text(answer['value']);
+
+            answerDiv.append(questionValue);
+
+            questionDiv.append(answerDiv);
+        });
+
+        questionsDiv.append(questionDiv);
+    });
+
+    console.log(results);
+    getSurveyResults();
 }
 
