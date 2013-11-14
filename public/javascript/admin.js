1e7b08f143ef1b22a1ae92b7998228a9d5b5c50f
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2013-11-14 13:20:48 -0600

    begin to refactor into single page app

--- public/javascript/admin.js
+++ public/javascript/admin.js
@@ -3,20 +3,7 @@
  */
 
 // eventually this plugin should be included in a global fashion
-// usage: $.QueryString["param"]
-(function($) {
-    $.QueryString = (function(a) {
-        if (a == "") return {};
-        var b = {};
-        for (var i = 0; i < a.length; ++i)
-        {
-            var p=a[i].split('=');
-            if (p.length != 2) continue;
-            b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
-        }
-        return b;
-    })(window.location.search.substr(1).split('&'))
-})(jQuery);
+
 
 
 $(function getSurveyInfo() {
--- public/javascript/site.js
+++ public/javascript/site.js
@@ -1,8 +1,33 @@
-// load the headers and footers
+// usage: $.QueryString["param"]
+(function($) {
+    $.QueryString = (function(a) {
+        if (a == "") return {};
+        var b = {};
+        for (var i = 0; i < a.length; ++i) {
+            var p=a[i].split('=');
+            if (p.length != 2) continue;
+            b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+        }
+        return b;
+    })(window.location.search.substr(1).split('&'))
+})(jQuery);
+
+
+// load the header, footer, content
 $(function() {
-  $.ajaxSetup({
-    async: false
-  });
-  $("#header").load("header.html");
-  $("#footer").load("footer.html");
+    $.ajaxSetup({
+        async: false
+    });
+    $("#header").load("header.html");
+    $("#footer").load("footer.html");
+
+    var page = $.QueryString["page"];
+
+    if(!page) {
+        // load home by default
+        $("#content").load("home.html");
+    } else {
+        $("#content").load(page + ".html");
+    }
+
 });
