6ce4955f7025b2d77f6e9c09e6f792f2a7bcd0e8
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2013-11-30 12:37:45 -0600

    add /deVote API endpoint (#127)

---
+++ routes/deVote.js
@@ -0,0 +1,56 @@
+var database = require("../modules/database");
+var httpresponses = require("../modules/httpresponses");
+var endpoint = require("../modules/endpoint");
+
+function deVote(){
+    var apiOptions = {};
+    
+    //The name of this route:
+    apiOptions.endpointName = "deVote";
+        
+    //Indicates the required API parameters and their basic expected types.
+    apiOptions.requiredApiParameters = {
+            "answerId":"number",
+            "questionId":"number"};
+    //Indicates the optional API parameters and their basic expected types.
+    apiOptions.optionalApiParameters = {
+            "surveyId":"number"};
+    
+    //Provides additional validation functions after the basic check on required parameters. 
+    //If a parameter is listed in this object, it MUST validate successfully and return true if provided in the request.
+    //In the case of a problem, return false or throw an error.
+    apiOptions.validators = {};
+    
+    //Function to execute if validation tests are successful.
+    apiOptions.conclusion = function(data, response) {
+        logger.info("Incoming deVote: " + data);
+        // TODO update with actual data (timestamp, uid, etc?)
+        database.deleteVote(data, function(err, results) {
+            if (err) {
+                if (!err["httpStatus"])
+                    err["httpStatus"] = 500;
+
+                if(!err["httpResponse"])
+                    err["httpResponse"] = "500 Internal Server Error";
+
+                if (!err["friendlyName"])
+                    err["friendlyName"] = "Error deleting vote";
+
+                httpresponses.errorResponse(err, response);
+                return;
+            }
+            else {
+                logger.info("Deleted vote from database");
+                httpresponses.successResponse(response);
+                return;
+            }
+        });
+    };
+    
+    var endpointObject = new endpoint.Endpoint(apiOptions);
+    return function() {  
+        (endpointObject.handle).apply(endpointObject, arguments);  
+    }; //return the handler function for the endpoint
+}
+
+exports.deVote = deVote;
