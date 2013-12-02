8af49538fc2a74e7421ec59f07898199a6d1f1af
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2013-12-02 17:29:19 -0600

    remove unused code from home.js

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -1,18 +1,3 @@
 // GOBAL PAGE VARIABLES //
 pageTitle = "Home";
 
-$(document).ready(function() {
-    $("#voter").submit(function(event) {
-        $.ajax({
-            type:"POST", 
-            url:"/vote", 
-            data:'{"vote": "a"}', 
-            contentType: 'application/json',
-            success: function(data) { console.log(data); },
-            error: function(data) { console.log(data); }
-		});
-        event.preventDefault();
-        return false; 
-    });
-});
-
