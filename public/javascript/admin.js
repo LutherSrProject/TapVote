7cff946016bf0a7302254d1013950e8cc4c25063
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2013-12-08 13:39:04 -0600

    remove question works in admin.html

--- public/javascript/admin.js
+++ public/javascript/admin.js
@@ -1,74 +1,90 @@
-/**
- * Created by isaac on 10/29/13.
- */
-
 // GLOBAL PAGE VARIABLES //
 pageTitle = "Admin";
 
 
 $(function getSurveyInfo() {
     var survey = $.QueryString['survey'];
-    $.ajax({
-        type:'GET',
-        url: '/getSurveyInfo',
-        data: {surveyId: survey},
-        success: displaySurveyInfo,
-        error: displayAjaxError
-    });
+    if (survey) {
+        $.ajax({
+            type:'GET',
+            url: '/getSurveyInfo',
+            data: {surveyId: survey},
+            success: displaySurvey,
+            error: displayAjaxError
+        });
+    } else {
+        askForSurveyId();
+    }
 });
 
-function getSurveyResults() {
-    var survey = $.QueryString['survey'];
-    $.ajax({
-        type:'GET',
-        url: '/getSurveyResults',
-        data: {surveyId: survey},
-        success: displaySurveyResults,
-        error: displayAjaxError
-    });
+function askForSurveyId() {
+    // This function is called when a survey Id isn't provided or when an invalid sId is given
+    var titleDiv = $("#survey-title");
+    titleDiv.text("Please enter a survey ID and click 'Edit Survey'.");
+
+    var idBox = $("<input id='survey-id' class='no-wrap survey-id-input-box' type='text' size=5 />");
+    var button = $("<button type='button' id='edit-survey-button'>Edit Survey</button>");
+    button.addClass("pure-button pure-button-success pure-button-small");
+    button.attr('onclick', 'redirectToSurvey()');
+
+    var form = $("<form></form>");
+    form.addClass("pure-form");
+    form.attr("action", "javascript:$('#edit-survey-button').click();");
+    form.append(idBox);
+    form.append(button);
+
+    var surveyDiv = $("#survey-questions");
+    surveyDiv.append(form);
+}
+
+function redirectToSurvey() {
+    window.location.href="/?p=admin&survey=" + $("#survey-id").val();
 }
 
 function displayAjaxError(error) {
-    console.log(error.statusText);
-    console.log(error.responseText);
-    var titleDiv = $("#survey-title");
-    titleDiv.text("Error getting survey or results. Did you specify a non-existent survey id?");
+    if (error.status == 404)
+        askForSurveyId(); // 404 happens when an invalid survey ID is specified
+    else
+        console.log(error);
 }
 
-function displaySurveyResults(results) {
-    $.each(results, function (key, value) {
-        var elementId = '#answer-' + key;
-        var resultSpan = $("<span></span>");
-        resultSpan.text(value);
-
-        $(elementId).append(resultSpan);
-    });
-    console.log(results);
-}
-
-function displaySurveyInfo(results) {
+function displaySurvey(results) {
     var titleDiv = $("#survey-title");
 
-    var el = $("<h2></h2>");
+    var el = $("<div></div>");
     el.text(results['title']);
+    el.prepend("<i class='fa fa-pencil' onclick='editSurveyTitle();'></i>");
+    el.addClass('survey-title');
     titleDiv.append(el);
 
     var questionsDiv = $("#survey-questions");
+    questionsDiv.addClass("questions");
+
     var questions = results['questions'];
     $.each(questions, function (index, question) {
         // create a div for each question in questions
         var questionDiv = $("<div></div>");
-        questionDiv.attr('id', 'question-'+question['id']);
-        questionDiv.attr('class', 'question');
+        questionDiv.attr('data-question-id', question['id']);
+        questionDiv.attr('class', 'question rounded');
+
+        var deleteQuestionButton = $("<button></button>");
+        deleteQuestionButton.attr("type", "button");
+        deleteQuestionButton.addClass("remove-question-button pure-button pure-button-error");
+        deleteQuestionButton.attr("onclick", "deleteQuestion(this);");
+        deleteQuestionButton.html("<i class='fa fa-times fa-lg'></i>");
+        questionDiv.append(deleteQuestionButton);
+
 
         var questionTitle = $("<div></div>");
         questionTitle.text(question['value']);
+        questionTitle.addClass('question-title');
         questionDiv.append(questionTitle);
 
         var answers = question['answers'];
         $.each(answers, function (index, answer) {
             var answerDiv = $("<div></div>");
-            answerDiv.attr('id', 'answer-'+answer['id']);
+            answerDiv.attr('data-question-id', question['id']);
+            answerDiv.attr('data-answer-id', answer['id']);
             answerDiv.attr('class', 'answer');
 
             var questionValue = $("<div></div>");
@@ -83,7 +99,28 @@ function displaySurveyInfo(results) {
     });
 
     console.log(results);
-    getSurveyResults();
+}
+
+function deleteQuestion(el) {
+    var target = $(el);
+    var questionDiv = target.parent();
+
+    var questionId = questionDiv.attr('data-question-id');
+
+    console.log(typeof parseInt(questionId));
+
+    $.ajax({
+        type:'POST',
+        url: '/removeQuestion',
+        data: JSON.stringify({questionId:parseInt(questionId)}),
+        contentType: 'application/json',
+        success: function(data) { questionDiv.remove() },
+        error: displayAjaxError
+    });
+}
+
+function editSurveyTitle() {
+    console.log("Changing survey title is not implemented");
 }
 
 
