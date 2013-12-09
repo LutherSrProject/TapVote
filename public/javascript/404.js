653d3def5dc78588f624760c438db6ff2acadc55
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2013-12-09 14:42:14 -0600

    404 should be standalone rather than dependent on index.html

--- public/javascript/404.js
+++ public/javascript/404.js
@@ -2,14 +2,23 @@
 pageTitle = "Page Not Found";
 
 
+$(function setup() {
+    $.ajaxSetup({
+        async: true
+    });
+    $("#header").load("header.html");
+    $("#footer").load("footer.html");
+});
+
 $(function guruMeditation() {
+
     var status = "404 Not Found";
     var url = window.location.href;
-    var referrer = document.referrer;
+    var referer = document.referrer;
 
     var text = "Status: " + status + "\n";
     text = text + "Current URL: " + url + "\n";
-    text = text + "Referrer: " + referrer + "\n";
+    text = text + "Referer: " + referer + "\n";
 
     $(".guru-meditation-code").text(text);
 });
