cc09f289ff0a504276ed91a2e60a46b825d6edd4
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2013-12-13 11:03:43 -0600

    some initial tests for deleteVote (#131)

--- tests/database.js
+++ tests/database.js
@@ -2,10 +2,10 @@
 require('../modules/globals');
 var Q = require("q");
 var should = require('should');
-
 var database = require('../modules/database');
-describe("Database", function(){
-    describe("createSurvey", function(){
+
+describe("Database", function () {
+    describe("createSurvey", function () {
         it("inserts a survey with questions and answers into the database", function(done) {
             database.createSurvey({ "title":"Test survey with questions and answers", "questions": [{"question": "Question 1", "type":"MCSR", "answers": ["Q1a1", "Q1a2", "Q1a3", "Q1a4"]}],"password":"supersecretpassword" },function(err,results) {
                 try {
@@ -79,9 +79,9 @@ describe("Database", function(){
             });
         });
     });
-    describe("recordVote", function(){
+    describe("recordVote", function () {
         it("inserts a valid vote into the database", function(done) {
-            database.recordVote({'answerId':1,'questionId':1},function(err,results) {
+            database.recordVote({'answerId':1,'questionId':1}, function(err,results) {
                 try {
                     should.not.exist(err);
                     done();
@@ -105,11 +105,45 @@ describe("Database", function(){
                 try {
                     should.exist(err,"expected to receive an error");
                     done();
-                } catch(testerror) {
+                } catch (testerror) {
                     done(testerror);
                 } 
             });
         });
+    });
+    describe("deleteVote", function () {
+        it("deletes a valid vote from the database", function (done) {
+            database.deleteVote({'answerId':1, 'questionId':1}, function (err, results) {
+                try {
+                    should.not.exist(err);
+                    // re-add that vote - otherwise we can only run the unit test suite a few times
+                    database.recordVote({'answerId':1, 'questionId':1});
+                    done();
+                } catch (testerror) {
+                    done(testerror);
+                }
+            })
+        });
+        it("deletes an invalid vote from the database", function (done) {
+            database.deleteVote({'answerId':1, 'questionId':2}, function (err, results) {
+                try {
+                    should.exist(err, "expected to receive an error");
+                    done();
+                } catch (testerror) {
+                    done(testerror);
+                }
+            })
+        });
+        it("deletes an invalid vote for an answer that doesn't exist from the database", function (done) {
+            database.deleteVote({'answerId':0, 'questionId':2}, function (err, results) {
+                try {
+                    should.exist(err, "expected to receive an error");
+                    done();
+                } catch (testerror) {
+                    done(testerror);
+                }
+            })
+        })
     });
     describe("getSurveyInfo", function(){
         it("gets a valid survey", function(done) {
