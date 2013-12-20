f1642d9716c58e2cb96b30d767c4cd65af60132a
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2013-12-20 14:38:20 -0600

    use moment to calculate the current time (for greater consistency in precision and formatting)

--- migrations/20131215224547-version5.js
+++ migrations/20131215224547-version5.js
@@ -1,9 +1,10 @@
 var dbm = require('db-migrate');
+var moment = require('moment');
 var type = dbm.dataType;
 
 exports.up = function(db, callback) {
     var queryString = "ALTER TABLE survey" +
-                      "    ADD COLUMN start timestamp DEFAULT (now() at time zone 'UTC')," +
+                      "    ADD COLUMN start timestamp DEFAULT ('" + moment.utc().format() + "')," +
                       "    ADD COLUMN finish timestamp DEFAULT '2300-01-01';";
 
     db.runSql(queryString, callback);
