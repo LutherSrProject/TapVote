bdc68f6b294fe9205b453c33690f78622098933b
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2014-02-20 14:42:03 -0600

    note that we're relying on addAnswer in the unit test for removeAnswer

--- tests/db-removeAnswer.js
+++ tests/db-removeAnswer.js
@@ -6,7 +6,7 @@ describe("removeAnswer", function(){
     it("inserts a new answer into the database, then removes it", function(done) {
         database.addAnswer({"questionId":1, "value":"new answer"}, function(err,results) {
             try {
-                should.not.exist(err);
+                should.not.exist(err, "addAnswer failed, so unable to test removeAnswer");
                 database.removeAnswer({"questionId":1, "answerId":results['answerId']}, function(err, results) {
                     should.not.exist(err);
                     done();
@@ -19,7 +19,7 @@ describe("removeAnswer", function(){
     it("inserts a new answer into the database, then tries to remove it with an incorrect questionId specified", function (done) {
         database.addAnswer({"questionId":1, "value":"new answer"}, function(err,results) {
             try {
-                should.not.exist(err);
+                should.not.exist(err, "addAnswer failed, so unable to test removeAnswer");
                 database.removeAnswer({"questionId":2, "answerId":results['answerId']}, function(err, results) {
                     should.exist(err);
                     done();
@@ -30,7 +30,7 @@ describe("removeAnswer", function(){
         });
     });
     it("tries to remove non-existent answer", function (done) {
-        database.removeAnswer({"questionId":2, "answerId":0}, function(err, results) {
+        database.removeAnswer({"questionId":1, "answerId":0}, function(err, results) {
             try {
                 should.exist(err);
                 done();
